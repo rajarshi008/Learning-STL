427460b3603e8efa3396148c15f4911adea05d76
Rajarshi Roy <rajarshi008@gmail.com>
2022-04-26 04:01:43 +0200

    Started converting to binary signals

--- learning.py
+++ learning.py
@@ -1,1 +1,68 @@
- 
+import Sample, samplePoint, Signal
+
+
+# read sample, predicates
+sample = Sample()
+sample.readSample()
+
+
+start_time = sample.positive[0].sequence[0].time # all signals start at same time
+end_time = sample.positive[0].sequence[-1].time # all signals end at same time
+
+binary_signals = {}
+interesting_time_points = {start_time,end_time}
+
+for signal in sample.positive:
+
+	for i in range(len(sample.vars)):
+		var = sample.vars[i]
+		
+		for c in sample.predicates[var]:
+			#Optimization: there is relation between the binary signals for same var
+
+			start_value = 1 if signal.sequence[0].vector[i] - c > 0 else 0
+			curr_binary_signal = binarySignal([samplePoint(time=starting_time, vector=[starting_value])])
+
+
+			for j in range(len(signal)-1):
+				
+				sp1 = signal.sequence[j]
+				sp2 = signal.sequence[j+1]
+				
+				t1 = sp1.time
+				var1 = sp1.vector[i]
+				
+				t2 = sp2.time
+				var2 = sp2.vector[i]
+
+				t0 = ((t2-t1)*c + var2*t1 - var1*t2)/(var2-var1)
+
+				if t0 < t1 or t0 > t2 or t0 == start_time or t0 == end_time:
+					continue
+				else:
+					interesting_time_points.add(t0)
+					curr_value = 1 - curr_binary_signal.sequence[-1].vector[0]
+					curr_binary_signal.addPoint(samplePoint(time=t0, vector=[curr_value])) 
+
+			end_value = 1 if signal.sequence[-1].vector[i] - c > 0 else 0
+			curr_binary_signal.addPoint(samplePoint(time=end_time, vector=[end_value]))
+
+
+			binary_signals[(signal,var,c)] = curr_binary_signal
+
+
+
+
+for signal in sample.positive:
+
+	binary
+	for i in range(len(sample.vars)):
+		var = sample.vars[i]
+		for c in sample.predicates[var]:
+			
+
+			binary_signals[(signal,var,c)]
+
+
+
+
--- signal.py
+++ signal.py
@@ -30,7 +30,34 @@ class samplePoint:
 		return text
 
 class Signal:
-	
+	'''
+	signals are finite piece-wise linear continuous functions
+	'''
+	def __init__(self, sequence):
+
+		if sequence==[] or isinstance(sequence[0], samplePoint):
+			self.sequence = sequence
+		else:
+			raise Exception("Invalid signals")
+
+	def addPoint(self, samplePoint):
+
+		self.sequence.append(samplePoint)
+
+	def calculateInfo(self):
+
+		self.domain = [i.time for i in self.sequence]
+
+	def __str__(self):
+
+		text = ';'.join([str(value) for value in self.sequence])
+		return text
+
+
+class binarySignal:
+	'''
+	binarySignals are finite piece-wise linear (possibly discontinuous) functions
+	'''
 	def __init__(self, sequence):
 
 		if sequence==[] or isinstance(sequence[0], samplePoint):
@@ -145,8 +172,128 @@ class Sample:
 
 
 
+class WordSample:
+	'''
+	contains the sample of postive and negative examples
+	'''
+	def __init__(self, positive=[], negative=[], alphabet=[], is_words=False, operators=['G', 'F', '!', 'U', '&','|', 'X']):
+
+		self.positive = positive
+		self.negative = negative
+		self.alphabet = alphabet
+		self.is_words = is_words
+		self.num_positives = len(self.positive)
+		self.num_negatives = len(self.negative)
+		self.operators=operators
+
+	
+	def extract_alphabet(self, is_word):
+		'''
+		extracts alphabet from the words/traces provided in the data
+		'''
+		alphabet = set()
+		
+		if self.is_words:
+			for w in self.positive+self.negative:
+				alphabet = alphabet.union(set(w.vector))
+			self.alphabet = list(alphabet)
+		else:
+			self.alphabet = [chr(ord('p')+i) for i in range(len(self.positive[0].vector[0]))] 
+
+	def word2trace(self, word):
+		one_hot_alphabet={}
+		for i in range(len(self.alphabet)):
+			one_hot_letter = [0]*len(self.alphabet)
+			letter = self.alphabet[i]
+			one_hot_letter[i] = 1
+			one_hot_alphabet[letter] = tuple(one_hot_letter)
+		trace_list=[]
+		for letter in word:
+			trace_list.append(one_hot_alphabet[letter])
+
+		return trace_list
 
 
 
+	def readFromFile(self, filename):
+		'''
+		reads .trace/.word files to extract sample from it
+		'''
+		self.is_words = ('.words' in filename)
+		with open(filename, 'r') as file:
+			mode = 0
+			count=0
+			while True:
+				count
+				line=file.readline()
+				if line=='':
+					break
+
+				if line == '---\n':
+					mode+=1
+					continue
+
+				if mode==0:	
+					# can read from both word file type and trace file type
+					if self.is_words:
+						word_vector, lasso_start = lineToWord(line)
+						word = Trace(vector=word_vector, lasso_start=lasso_start, is_word=True)	 	
+						self.positive.append(word)
+					else:
+						trace_vector, lasso_start = lineToTrace(line)
+						trace = Trace(vector=trace_vector, lasso_start=lasso_start, is_word=False)	 	
+						self.positive.append(trace)
+
+				if mode==1:
+					
+					if self.is_words:
+						word_vector, lasso_start = lineToWord(line)
+						word = Trace(vector=word_vector, lasso_start=lasso_start, is_word=True)	 	
+						self.negative.append(word)
+					else:
+						trace_vector, lasso_start = lineToTrace(line)
+						trace = Trace(vector=trace_vector, lasso_start=lasso_start, is_word=False)	 	
+						self.negative.append(trace)
+
+				if mode==2:
+					self.operators = list(line.strip().split(','))
+				if mode==3:
+					self.alphabet = list(line.split(','))
+
+
+		if mode != 3:		
+				self.extract_alphabet(self.is_words)
+		
+		self.letter2pos={}
+		for i in range(len(self.alphabet)):
+			self.letter2pos[self.alphabet[i]]=i
+		
+		if self.is_words:
+			for word in self.positive+ self.negative:
+				word.vector= self.word2trace(word.vector)
+				word.vector_str= str(word.vector)
+				word.is_word = False
+
+		self.writeToFile('small-example')
+
+	def writeToFile(self, filename):
+
+		with open(filename, 'w') as file:
+			for trace in self.positive:
+
+				file.write(str(trace)+'\n')
+			file.write('---\n')
+
+			for trace in self.negative:
+				file.write(str(trace)+'\n')
+
+
+			if self.operators!=[]:
+				file.write('---\n')
+				file.write(','.join(self.operators)+'\n')
+
+			if self.alphabet != []:
+				file.write('---\n')
+				file.write(','.join(self.alphabet))
 
 
