212f57fca5ef52f89abbb443221d859483ba1219
Rajarshi Roy <rajarshi008@gmail.com>
2022-11-23 17:01:52 +0100

    Added code for interval merging

--- merging.py
+++ merging.py
@@ -5,7 +5,7 @@ class SMTEncoding:
 
 	def __init__(self):
 
-		self.max_intervals = 9
+		self.max_intervals = 10
 		self.start_time = 0
 		self.end_time = 20
 
@@ -13,17 +13,21 @@ class SMTEncoding:
 
 	def def_vars(self):
 
-		self.s = {i:Real('t_%d'%i) for i in range(1,self.max_intervals+1)}#dummy variables
+		self.initial_tp = [0, 4, 2, 7, 6, 10, 9, 12, 20, 20, 20]
+		self.max_intervals = len(self.initial_tp)
+		self.start_time = self.initial_tp[0]
+		self.end_time = self.initial_tp[-1]
+
+		self.s = {i:Real('s_%d'%i) for i in range(1,self.max_intervals+1)}#dummy variables
 		self.t = {i:Real('t_%d'%i)for i in range(1,self.max_intervals+1)}#actual variables
 		self.num_tp = Int('r')
 
 	def constraints(self):
 
-		initial_tp = [0, 4, 2, 6, 7, 10, 9, 11, 20]
-		#[0,6,7,11,20]
+		
+		#[0,6,7,10, 11, 12, 20,20,20]
 
-		self.solver.assert_and_track(And([self.s[i] == initial_tp[i-1] for i in range(1,self.max_intervals+1)]), 'Inital timepoints')
-
+		
 		if self.max_intervals%2 != 0:
 			even_range = int((self.max_intervals-1)/2)
 			odd_range = int((self.max_intervals+1)/2)
@@ -31,39 +35,53 @@ class SMTEncoding:
 			even_range = int((self.max_intervals)/2)
 			odd_range = int((self.max_intervals)/2)
 
+		initial_cons = And([self.s[i] == self.initial_tp[i-1] for i in range(1,self.max_intervals+1)])
+		self.solver.assert_and_track(initial_cons, 'Inital timepoints')
 
-		interval_bound = And(1<=self.num_tp, self.num_tp<=self.max_intervals)
+
+		interval_bound = And(1<self.num_tp, self.num_tp<=self.max_intervals)
 		self.solver.assert_and_track(interval_bound, 'Bound for number of relevant time points')
 
-		cons1 = And([Or([self.t[2*i] == self.s[2*j] \
+		end_point_bound = And([Implies(i>=self.num_tp, self.t[i] == self.end_time) \
+								for i in range(1,self.max_intervals+1)])
+		self.solver.assert_and_track(end_point_bound, 'End time points')
+
+		
+		cons1 = And([Or([Implies(2*i<=self.num_tp,self.t[2*i] == self.s[2*j]) \
 						for j in range(1, even_range+1)]) \
 						for i in range(1, even_range+1)])
 		self.solver.assert_and_track(cons1, 'Even variables are set to one of the even dummy variables')
 
-		cons2 = And([Or([self.t[2*i-1] == self.s[2*j-1] \
+		cons2 = And([Or([Implies(2*i-1<=self.num_tp, self.t[2*i-1] == self.s[2*j-1]) \
 						for j in range(1, odd_range+1)]) \
 						for i in range(1, odd_range+1)])
 		self.solver.assert_and_track(cons2 , 'Odd variables are set to one of the odd dummy variables')
+		
+		#cons = And([Or([self.t[i] == self.s[j] \
+		#				for j in range(1, self.max_intervals+1)]) \
+		#				for i in range(1, self.max_intervals+1)])
+		#self.solver.assert_and_track(cons , 'Actual variables are set to one of the dummy variables')
 
-		cons3 = And([self.t[i]<self.t[i+1] \
+		cons3 = And([Implies(i<self.num_tp, self.t[i]<self.t[i+1]) \
 						for i in range(1,self.max_intervals)])
 		self.solver.assert_and_track(cons3 , 'Maintain order between variables')	
-
-		cons4 = And([Or([And(self.t[j]<=self.s[i], self.s[i]<=self.t[j+1]) \
-							for j in range(1, self.max_intervals)]) \
-							for i in range(1, self.max_intervals+1)])
-		self.solver.assert_and_track(cons4 , 'All dummy variables should be included')
+		
+		cons4 = And([Or([And(self.t[2*j-1]<=self.s[2*i-1], self.s[2*i]<=self.t[2*j]) \
+							for j in range(1, even_range+1)]) \
+							for i in range(1, even_range+1)])
+		self.solver.assert_and_track(cons4 , 'All intervals must be included')
 
 
 		cons5 = And([Implies(self.s[2*i]<self.s[2*j-1], \
-					Implies(And([Or((self.s[2*i]+self.s[2*j-1])/2<self.s[k], (self.s[2*i]+self.s[2*j-1])/2>self.s[k+1]) \
-						for k in range(1, self.max_intervals)]),\
-						And([Or((self.s[2*i]+self.s[2*j-1])/2<self.t[l], (self.s[2*i]+self.s[2*j-1])/2>self.t[l+1]) for l in range(1,self.max_intervals)])
+					Implies(And([Or((self.s[2*i]+self.s[2*j-1])/2<self.s[2*k-1], (self.s[2*i]+self.s[2*j-1])/2>self.s[2*k]) \
+						for k in range(1, even_range)]),\
+						And([Or((self.s[2*i]+self.s[2*j-1])/2<self.t[2*l-1], (self.s[2*i]+self.s[2*j-1])/2>self.t[2*l]) for l in range(1,even_range)])
 						)\
-					)\
 					for i in range(1, even_range+1) for j in range(1, odd_range+1)\
 					])
 		self.solver.assert_and_track(cons5 , 'No extra variables should be included')
+		
+
 
 	def find_sol(self):
 
@@ -76,7 +94,8 @@ class SMTEncoding:
 				print('t_%d'%k, solverModel[self.t[k]])
 			print('Num TP', solverModel[self.num_tp])
 		else:
-
+			checking= self.solver.unsat_core()
+			print(checking)
 			print('UNSAT')
 	
 
